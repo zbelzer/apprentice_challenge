f4c095a6f77de3b66d44535a58d4cc5eb2b62d9b
Zachary Belzer <zbelzer@gmail.com>
2014-01-03 13:05:38 -0600

    Re-read challenge again... make api conform to requirements

--- app.rb
+++ app.rb
@@ -2,22 +2,31 @@
 
 require_relative 'lib/record_parser'
 require_relative 'lib/record_printer'
-require_relative 'lib/server'
 
-parser = RecordParser.new(ARGV[0])
+if ARGV.empty?
+  $stderr.puts "./app.rb <file> [<file>, ...]" 
+  exit 1
+end
+
+files = ARGV
+all_rows = ARGV.inject([]) do |rows, file|
+  parser = RecordParser.new(file)
+  rows += parser.parse
+end
+
 print_order = %w(LastName FirstName Gender DateOfBirth FavoriteColor)
 
-puts "By Gender asendingc then LastName descending"
-rows = parser.parse(:sort => [[:Gender, :asc], [:LastName, :asc]])
-puts RecordPrinter.new(rows).print(*print_order)
+puts "By Gender asending then LastName descending"
+sorted_rows = RecordSorter.new(all_rows).sort([[:Gender, :asc], [:LastName, :asc]])
+puts RecordPrinter.new(sorted_rows).print(*print_order)
 puts
 
 puts "By DateOfBirth ascending"
-rows = parser.parse(:sort => [[:LastName, :desc]])
-puts RecordPrinter.new(rows).print(*print_order)
+sorted_rows = RecordSorter.new(all_rows).sort([[:LastName, :desc]])
+puts RecordPrinter.new(sorted_rows).print(*print_order)
 puts
 
 puts "By LastName descending"
-rows = parser.parse(:sort => [[:LastName, :desc]])
-puts RecordPrinter.new(rows).print(*print_order)
+sorted_rows = RecordSorter.new(all_rows).sort([[:LastName, :desc]])
+puts RecordPrinter.new(sorted_rows).print(*print_order)
 puts
