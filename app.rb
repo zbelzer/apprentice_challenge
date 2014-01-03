32741aebe522114726b75042d192bd9e552faeef
Zachary Belzer <zbelzer@gmail.com>
2014-01-02 21:31:54 -0600

    App works, formatting is wrong

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/env ruby
 
-file = nil
-options = {}
-RecordParser.parse(file, options)
+require_relative 'lib/record_parser'
+
+parser = RecordParser.new(ARGV[0])
+
+puts "By Gender asendingc then LastName descending"
+puts parser.parse(:sort => [[:Gender, :asc], [:LastName, :asc]])
+puts
+
+puts "By DateOfBirth ascending"
+puts parser.parse(:sort => [[:LastName, :desc]])
+puts
+
+puts "By LastName descending"
+puts parser.parse(:sort => [[:LastName, :desc]])
