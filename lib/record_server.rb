e20e1170980807bd57c090ed02c601a61caedeb5
Zachary Belzer <zbelzer@gmail.com>
2014-01-03 12:04:45 -0600

    Get tests to pass

--- lib/record_server.rb
+++ lib/record_server.rb
@@ -1,12 +1,23 @@
 require 'grape'
+require 'tempfile'
 
 class RecordServer < Grape::API
   format :json
 
-  helpers do
-    def records
-      @records ||= []
-    end
+  # Get internal data store of records.
+  def self.records
+    @records
+  end
+
+  # Add new records to store.
+  def self.add_records(new_records)
+    @records ||= []
+    @records += new_records
+  end
+
+  # Reset store.
+  def self.reset
+    @records = nil
   end
 
   resource :records do
@@ -15,12 +26,17 @@ class RecordServer < Grape::API
       requires :data, type: String, desc: "A line of delimited data"
     end
     post do
-      records
+      tempfile = Tempfile.new("temp")
+      tempfile.write params[:data]
+      tempfile.rewind
+
+      parser = RecordParser.new(tempfile.path)
+      RecordServer.add_records parser.parse
     end
 
     desc "Returns records sorted by {sort}"
     get do
-      records
+      RecordServer.records
     end
   end
 end
--- spec/record_server_spec.rb
+++ spec/record_server_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe RecordServer do
-  let(:einstein) { {:LastName => "Einstein", :FirstName => "Albert",  :Gender => "Male",   :FavoriteColor => "Green",  :DateOfBirth => Date.parse("1879-03-14")} }
-
   include Rack::Test::Methods
+
+  let(:einstein) { {:LastName => "Einstein", :FirstName => "Albert",  :Gender => "Male",   :FavoriteColor => "Green",  :DateOfBirth => Date.parse("1879-03-14")} }
 
   def app
     RecordServer
+  end
+
+  before do
+    app.reset
   end
 
   describe "POST /records" do
@@ -14,14 +18,15 @@ describe RecordServer do
       einstein = sample_from_fixture('test.csv', 1)
       post_json "/records", {:data => einstein}
 
-      last_response.status.should == 200
-      expect(json_response).to have(1).record
-      json_response[0].should == einstein.to_json
+      last_response.status.should == 201
     end
   end
 
   describe "GET /records" do
     it "gets all records" do
+      einstein = sample_from_fixture('test.csv', 1)
+      post_json "/records", {:data => einstein}
+
       get_json "/records"
       last_response.status.should == 200
       expect(json_response).to have(1).record
