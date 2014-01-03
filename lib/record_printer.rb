e853a70f874cd5ec286e3beed519adf94cf7d4d3
Zachary Belzer <zbelzer@gmail.com>
2014-01-02 21:58:39 -0600

    Make some expectations on output

--- lib/record_printer.rb
+++ lib/record_printer.rb
@@ -1,5 +1,10 @@
 # Class used to print rows of hashes
 class RecordPrinter
+  # Column separator
+  COL_SEP = ","
+
+  # Row separator
+  ROW_SEP = "\n"
 
   # Create a new RecordPrinter with given rows
   def initialize(rows)
@@ -7,7 +12,12 @@ class RecordPrinter
   end
 
   # Print the rows with the columns in the given order
-  def print(columns)
-    @rows
+  def print(*columns)
+    rows = [columns.join(COL_SEP)]
+    rows += @rows.map do |row|
+      columns.map { |col| row[col.to_sym] }.join(COL_SEP)
+    end
+    
+    rows.join(ROW_SEP)
   end
 end
--- spec/record_printer_spec.rb
+++ spec/record_printer_spec.rb
@@ -1,11 +1,18 @@
 require_relative 'spec_helper'
 
 describe RecordPrinter do
+  let(:rows) do
+    [
+      {:a => 1, :b => 2, :c => 3},
+      {:a => 4, :b => 5, :c => 6},
+    ]
+  end
+
   describe "print" do
-    it "does not fail" do
-      rows = []
+    it "sorts by given columns" do
       printer = RecordPrinter.new(rows)
-      printer.print({})
+      result = printer.print(:b, :c, :a)
+      expect(result).to eq("b,c,a\n2,3,1\n5,6,4")
     end
   end
 end
